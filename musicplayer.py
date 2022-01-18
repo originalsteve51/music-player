8f6047522a0c17ed03d47c1dafd481fbdf0ac73b
Stephen Harding <ssh751@gmail.com>
2022-01-18 12:37:55 -0500

    Add PlaybackInProgressError

--- musicplayer.py
+++ musicplayer.py
@@ -27,6 +27,17 @@ class NoPlaybackError(Exception):
     """
     pass 
 
+
+#-------------------------------------------------------------------
+# PlaybackInProgressError class
+#-------------------------------------------------------------------
+class PlaybackInProgressError(Exception):
+    """
+    When an attempt is made to begin playback while something
+    is already playing, this Exception is raised.
+    """
+    pass 
+
 #-------------------------------------------------------------------
 # MusicPlayer class
 #-------------------------------------------------------------------
@@ -39,46 +50,60 @@ class MusicPlayer():
         self._audioengine = 'mpg123' # Only supported player is mpg123
         self._p = None
         self._is_paused = False
+
+        # The following instance variables are affected by the process_monitor
+        # thread. 
         self._process_running = False
         self._return_code = None
 
 
     def play(self, sound_file):
-        try:
-            self._p = subprocess.Popen([self._audioengine, 
-                                  '-C',     # Enable commands to be read from stdin
-                                  '-q',     # Be quiet
-                                  sound_file],
-                                  stdin=subprocess.PIPE, # Pipe input via bytes
-                                  stdout=None,   
-                                  stderr=None)
+        if not self._process_running:
+            try:
+                # Open a subprocess that runs mpg123 to play an mp3 file
+                self._p = subprocess.Popen([self._audioengine, 
+                                      '-C',     # Enable commands to be read from stdin
+                                      '-q',     # Be quiet
+                                      sound_file],
+                                      stdin=subprocess.PIPE, # Pipe input via bytes
+                                      stdout=None,   
+                                      stderr=None)
 
-            # Since we are using stdin for commands, we have to send something
-            # to keep mpg123 from complaining when we exit. The complaint is
-            # not a serious one, but it is annoying. If stdin is not used the
-            # terminal is posted with "Can't set terminal attributes" at exit.
-            # I send an empty string below to keep mpg123 happy.
-            self._p.stdin.write(b'')
-            self._p.stdin.flush()
+                # Since we are using stdin for commands, we have to send something
+                # to keep mpg123 from complaining when we exit. The complaint is
+                # not a serious one, but it is annoying. If stdin is not used the
+                # terminal is posted with "Can't set terminal attributes" at exit.
+                # I send an empty string below to keep mpg123 happy.
+                self._p.stdin.write(b'')
+                self._p.stdin.flush()
 
-            monitor_thread = Thread(target=self.process_monitor,args=()) 
-            monitor_thread.start()
+                # start a monitor thread that sets instance variables with the
+                # status of the subprocess. 
+                monitor_thread = Thread(target=self.process_monitor,args=()) 
+                monitor_thread.start()
 
-        except FileNotFoundError as e:
-            raise AudioEngineUnavailableError(f'AudioEngineUnavailableError: {e}')
+            except FileNotFoundError as e:
+                raise AudioEngineUnavailableError(f'AudioEngineUnavailableError: {e}')
+        else:
+            raise PlaybackInProgressError('You cannot play while something else is already playing.')
 
     def quit_playing(self):
-        self._p.stdin.write(b'q')
-        self._p.stdin.flush()
+        if self._process_running:
+            self._p.stdin.write(b'q')
+            self._p.stdin.flush()
 
-        # Wait for process to end
-        while self._process_running:
-            time.sleep(0.1)
+            # Wait for process to end
+            while self._process_running:
+                time.sleep(0.1)
+        else:
+            raise NoPlaybackError('Cannot quit playback because nothing is playing.')
+
 
 
     def pause(self):
         if self._process_running:
             if self._is_paused:
+                # Already paused, do nothing
                 pass
             else:
                 self._p.stdin.write(b's')
@@ -91,6 +116,7 @@ class MusicPlayer():
     def resume(self):
         if self._process_running:
             if not self._is_paused:
+                # Already playing, do nothing
                 pass
             else:
                 self._p.stdin.write(b's')
@@ -106,6 +132,11 @@ class MusicPlayer():
         return self._return_code
 
     def process_monitor(self):
+        """
+        This code runs in its own thread to monitor the state of our
+        external subprocess. Instance variables _process_running and
+        _return_code are used to show the process status.
+        """
         # Indicate that the process is running at the start, it
         # should be
         self._process_running = True
@@ -134,9 +165,14 @@ def main():
 
     player.play('./1968_top_ten/mony.mp3')
     print(f'Player is playing: {player.is_playing()}')
+    print('Play for 2 seconds')
     time.sleep(2)
+
+    print('Pause player for 2 seconds')
     player.pause()
     time.sleep(2)
+
+    print('')
     player.resume()
     time.sleep(2)
 
